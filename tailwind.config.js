064a86c78d5c2fc471fcd7636c89605008027818
Matias <matuzapata@gmail.com>
2021-10-04 22:36:43 -0300

    Funcional base

---
+++ tailwind.config.js
@@ -0,0 +1,146 @@
+module.exports = {
+  // mode: 'jit',
+  // purge: ['./content/**/*.md', './pages/**', './pages/**/*.vue', './components/**'],
+  darkMode: false, // or 'media' or 'class '
+  variants: {},
+  plugins: [
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Theme colors
+        primary: 'var(--bs-primary)',
+        secondary: 'var(--bs-secondary)',
+        success: 'var(--bs-success)',
+        info: 'var(--bs-info)',
+        warning: 'var(--bs-warning)',
+        danger: 'var(--bs-danger)',
+        light: 'var(--bs-light)',
+        dark: 'var(--bs-dark)',
+        // Pallete
+        gray: {
+          100: 'var(--bs-gray-100)',
+          200: 'var(--bs-gray-200)',
+          300: 'var(--bs-gray-300)',
+          400: 'var(--bs-gray-400)',
+          500: 'var(--bs-gray-500)',
+          600: 'var(--bs-gray-600)',
+          700: 'var(--bs-gray-700)',
+          800: 'var(--bs-gray-800)',
+          900: 'var(--bs-gray-900)'
+        },
+        blue: {
+          100: 'var(--bs-blue-100)',
+          200: 'var(--bs-blue-200)',
+          300: 'var(--bs-blue-300)',
+          400: 'var(--bs-blue-400)',
+          500: 'var(--bs-blue-500)',
+          600: 'var(--bs-blue-600)',
+          700: 'var(--bs-blue-700)',
+          800: 'var(--bs-blue-800)',
+          900: 'var(--bs-blue-900)'
+        },
+        indigo: {
+          100: 'var(--bs-indigo-100)',
+          200: 'var(--bs-indigo-200)',
+          300: 'var(--bs-indigo-300)',
+          400: 'var(--bs-indigo-400)',
+          500: 'var(--bs-indigo-500)',
+          600: 'var(--bs-indigo-600)',
+          700: 'var(--bs-indigo-700)',
+          800: 'var(--bs-indigo-800)',
+          900: 'var(--bs-indigo-900)'
+        },
+        purple: {
+          100: 'var(--bs-purple-100)',
+          200: 'var(--bs-purple-200)',
+          300: 'var(--bs-purple-300)',
+          400: 'var(--bs-purple-400)',
+          500: 'var(--bs-purple-500)',
+          600: 'var(--bs-purple-600)',
+          700: 'var(--bs-purple-700)',
+          800: 'var(--bs-purple-800)',
+          900: 'var(--bs-purple-900)'
+        },
+        pink: {
+          100: 'var(--bs-pink-100)',
+          200: 'var(--bs-pink-200)',
+          300: 'var(--bs-pink-300)',
+          400: 'var(--bs-pink-400)',
+          500: 'var(--bs-pink-500)',
+          600: 'var(--bs-pink-600)',
+          700: 'var(--bs-pink-700)',
+          800: 'var(--bs-pink-800)',
+          900: 'var(--bs-pink-900)'
+        },
+        red: {
+          100: 'var(--bs-red-100)',
+          200: 'var(--bs-red-200)',
+          300: 'var(--bs-red-300)',
+          400: 'var(--bs-red-400)',
+          500: 'var(--bs-red-500)',
+          600: 'var(--bs-red-600)',
+          700: 'var(--bs-red-700)',
+          800: 'var(--bs-red-800)',
+          900: 'var(--bs-red-900)'
+        },
+        orange: {
+          100: 'var(--bs-orange-100)',
+          200: 'var(--bs-orange-200)',
+          300: 'var(--bs-orange-300)',
+          400: 'var(--bs-orange-400)',
+          500: 'var(--bs-orange-500)',
+          600: 'var(--bs-orange-600)',
+          700: 'var(--bs-orange-700)',
+          800: 'var(--bs-orange-800)',
+          900: 'var(--bs-orange-900)'
+        },
+        yellow: {
+          100: 'var(--bs-yellow-100)',
+          200: 'var(--bs-yellow-200)',
+          300: 'var(--bs-yellow-300)',
+          400: 'var(--bs-yellow-400)',
+          500: 'var(--bs-yellow-500)',
+          600: 'var(--bs-yellow-600)',
+          700: 'var(--bs-yellow-700)',
+          800: 'var(--bs-yellow-800)',
+          900: 'var(--bs-yellow-900)'
+        },
+        green: {
+          100: 'var(--bs-green-100)',
+          200: 'var(--bs-green-200)',
+          300: 'var(--bs-green-300)',
+          400: 'var(--bs-green-400)',
+          500: 'var(--bs-green-500)',
+          600: 'var(--bs-green-600)',
+          700: 'var(--bs-green-700)',
+          800: 'var(--bs-green-800)',
+          900: 'var(--bs-green-900)'
+        },
+        teal: {
+          100: 'var(--bs-teal-100)',
+          200: 'var(--bs-teal-200)',
+          300: 'var(--bs-teal-300)',
+          400: 'var(--bs-teal-400)',
+          500: 'var(--bs-teal-500)',
+          600: 'var(--bs-teal-600)',
+          700: 'var(--bs-teal-700)',
+          800: 'var(--bs-teal-800)',
+          900: 'var(--bs-teal-900)'
+        },
+        cyan: {
+          100: 'var(--bs-cyan-100)',
+          200: 'var(--bs-cyan-200)',
+          300: 'var(--bs-cyan-300)',
+          400: 'var(--bs-cyan-400)',
+          500: 'var(--bs-cyan-500)',
+          600: 'var(--bs-cyan-600)',
+          700: 'var(--bs-cyan-700)',
+          800: 'var(--bs-cyan-800)',
+          900: 'var(--bs-cyan-900)'
+        }
+
+      }
+    }
+  }
+}
