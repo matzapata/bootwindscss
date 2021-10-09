64dadf9562dc66ec0e944d30051d6c6df1493195
Matias <matuzapata@gmail.com>
2021-10-09 19:37:03 -0300

    Removed dark mode

--- docs/Dependencies/theme-docs/src/components/global/app/AppColorSwitcher.vue
+++ docs/Dependencies/theme-docs/src/components/global/app/AppColorSwitcher.vue
@@ -1,7 +1,8 @@
 <template>
-  <span class="inline-flex">
+<div>
+  <!-- <span class="inline-flex">
     <button
-      class="rounded-md hover:text-primary-500 focus:outline-none transition ease-in-out duration-150"
+      class="transition duration-150 ease-in-out rounded-md hover:text-primary-500 focus:outline-none"
       aria-label="Color Mode"
       @click="$colorMode.value === 'dark' ? $colorMode.preference = 'light' : $colorMode.preference = 'dark'"
     >
@@ -10,5 +11,6 @@
         <IconMoon v-else class="w-6 h-6" />
       </client-only>
     </button>
-  </span>
+  </span> -->
+</div>
 </template>
