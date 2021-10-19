bf0c6a1a292119c163fdbaefab9e8756ee2cbce1
Matias <matuzapata@gmail.com>
2021-10-19 10:42:20 -0300

    Fix nuxt docs theme conflicts and styles

--- docs/Dependencies/theme-docs/src/components/app/AppToc.vue
+++ docs/Dependencies/theme-docs/src/components/app/AppToc.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="toc.length" class="w-full lg:w-1/4 block relative">
+  <div v-if="toc.length" class="relative block w-full lg:w-1/4">
     <div class="lg:sticky lg:top-16 overflow-y-auto h-full lg:h-auto lg:max-h-(screen-16)">
       <nav
         class="py-4 lg:py-8"
@@ -9,7 +9,7 @@
         }"
       >
         <p
-          class="mb-3 lg:mb-2 text-gray-500 uppercase tracking-wider font-bold text-sm lg:text-xs"
+          class="mb-3 text-sm font-bold tracking-wider text-gray-500 uppercase lg:mb-2 lg:text-xs"
         >{{ $t('toc.title') }}</p>
         <scrollactive highlight-first-item active-class="text-primary-500" :offset="0" tag="ul">
           <li
@@ -22,7 +22,7 @@
           >
             <a
               :href="`#${link.id}`"
-              class="block text-sm scrollactive-item transition-padding ease-in-out duration-300 hover:pl-1"
+              class="block text-sm duration-300 ease-in-out scrollactive-item transition-padding hover:pl-1"
               :class="{
                 'py-2': link.depth === 2,
                 'ml-2 pb-2': link.depth === 3,
--- docs/Dependencies/theme-docs/src/components/global/base/List.vue
+++ docs/Dependencies/theme-docs/src/components/global/base/List.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div v-for="(item, i) in items" :key="i" class="mt-3 flex">
-      <span :class="`list-${type}`" class="mt-px mr-3 flex-shrink-0">
-        <component :is="iconName" class="h-6 w-6" />
+    <div v-for="(item, i) in items" :key="i" class="flex mt-3">
+      <span :class="`list-${type}`" class="flex-shrink-0 mt-px mr-3">
+        <component :is="iconName" class="w-6 h-6" />
       </span>
       {{ item }}
     </div>
